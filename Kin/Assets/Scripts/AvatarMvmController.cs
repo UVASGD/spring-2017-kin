26be98594f2003ae50264d7dd60d270d95bd003f
ferociouself <jhe3cd@virginia.edu>
2016-08-27 13:08:27 -0400

    Basic Animations
    Added walking animation and idle animation for forward walking. Right
    now transitions are based on a bool but eventually it will be based on
    floats for horizontal and vertical movement.

--- Kin/Assets/Scripts/AvatarMvmController.cs
+++ Kin/Assets/Scripts/AvatarMvmController.cs
@@ -8,6 +8,7 @@ public class AvatarMvmController : MonoBehaviour {
     void Update()
     {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		gameObject.GetComponent<Animator>().SetBool("Moving", move.magnitude > 0);
         transform.position += move * speed * Time.deltaTime;
     }
 }
