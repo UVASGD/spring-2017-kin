47c240527905811f4bf9f4851b356c578ed78bdc
Zachary Danz <zsd4yr@virginia.edu>
2016-08-27 12:45:55 -0300

    StaticMethods + Camera Work
    Thx Jackson

---
+++ Kin/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject playerChar;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.position = Vector3.Lerp(getLerpA(), 
+            StaticMethods.ChangeZ(playerChar.transform.position, this.gameObject.transform.position.z), 
+            vTot(playerChar.GetComponent<Rigidbody2D>().velocity.magnitude));
+	}
+
+    float vTot (float v)
+    {
+        return 1 / (v + 1);
+    }
+
+    Vector3 getLerpA()
+    {
+        return StaticMethods.ChangeZ(playerChar.transform.position, this.gameObject.transform.position.z)
+            - StaticMethods.V2toV3(playerChar.GetComponent<Rigidbody2D>().velocity, this.gameObject.transform.position.z);
+    }
+}
