4573135ac5cb53dc740beb4b476e51504f316888
Jared Capelle <jjc2bd@virginia.edu>
2016-09-17 15:29:21 -0400

    Added Sets and Gets for health and stamina sliders

--- Kin/Assets/Scripts/UIController.cs
+++ Kin/Assets/Scripts/UIController.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SliderController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Slider health;
+    public Slider stamina;
+    // Add clock
+    // Add Boss Health
 	
+	void Start () {
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void setMaxHealth(int maxHealth)
+    {
+        health.maxValue = maxHealth;
+    }
+
+    void setMaxStamina(int maxStam)
+    {
+        stamina.maxValue = maxStam;
+    }
+
+    void setStamina(int val)
+    {
+        stamina.value = val;
+    }
+
+    void setHealth(int val)
+    {
+
+        health.value = val;
+    }
 }
