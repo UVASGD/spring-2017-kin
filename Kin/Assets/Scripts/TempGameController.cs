8bbffc08ece8302832d6c6df9060774ebb4d1cf7
ferociouself <jhe3cd@virginia.edu>
2016-09-24 14:55:25 -0400

    Fixed game controller
    Save Controller not singleton

--- Kin/Assets/Scripts/TempGameController.cs
+++ Kin/Assets/Scripts/TempGameController.cs
@@ -5,19 +5,21 @@ using System.Collections;
 public class TempGameController : MonoBehaviour {
 
     public Canvas canv;
-    private SaveController save = SaveController.s_instance;
+	public GameObject save;
+    private SaveController saveCont;
     private UIController ui;
 
 	// Use this for initialization
 	void Start () {
+		saveCont = save.GetComponent<SaveController>();
         ui = canv.GetComponent<UIController>();
-        ui.setMaxHealth((int)save.health);
-        ui.setMaxStamina((int)save.stamina);
+        ui.setMaxHealth((int)saveCont.health);
+        ui.setMaxStamina((int)saveCont.stamina);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ui.setHealth((int)save.health);
-        ui.setStamina((int)save.stamina);
+        ui.setHealth((int)saveCont.health);
+        ui.setStamina((int)saveCont.stamina);
 	}
 }
