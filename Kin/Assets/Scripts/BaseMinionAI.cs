48c4d052160605c433f0afde0c7babb9c6804bef
gtg3vv <gtg3vv@virginia.edu>
2016-09-24 14:03:32 -0400

    Basic Projectiles
    added minionai class
    added projectile class

---
+++ Kin/Assets/Scripts/BaseMinionAI.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BaseMinionAI : BaseAI {
+
+	//Ai Damage Values
+	float projectileDamage;
+	float meleeDamage;
+	//Pause between minion attacks
+	float attackMeleeDelay;
+	float fireProjDelay;
+
+	float attackRange;
+	float projectileRange;
+	public Object prefab;
+	float curCd;
+	bool onCd;
+
+
+	AIStates curstate;
+
+
+	// Use this for initialization
+	protected override void Start () {
+		onCd = false;
+		curCd = 0.0f;
+		Debug.Log ("in sub start");
+		base.rb = gameObject.GetComponent<Rigidbody2D> ();
+		curstate = BaseAI.AIStates.DetectedState;
+		//prefab = AssetDatabase.LoadAssetAtPath("Assets/prefabs.MinionProj", typeof(GameObject));
+
+	
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		if (curstate == BaseAI.AIStates.DetectedState) {
+			Debug.Log ("Calling move");
+			MoveTowardsTarget ();
+			if (!onCd) {
+				fireProj ();
+				curCd = .5f;
+				onCd = true;
+			} else {
+				curCd -= Time.deltaTime;
+				if (curCd <= 0.0f)
+					onCd = false;
+			}
+	
+		}
+	}
+
+	protected void fireProj()
+	{
+		Vector2 loc = new Vector2(targetObject.transform.position.x,targetObject.transform.position.y);
+		GameObject newProj = (GameObject) GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+		newProj.GetComponent<Rigidbody2D> ().velocity = (targetObject.transform.position - gameObject.transform.position).normalized * 1f;
+		Debug.Log (newProj.transform.position);
+
+
+	}
+
+
+
+
+}
---
+++ Kin/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed = 5.0f;
+	//Range at which proj will decay
+	public float range = 500.0f;
+	public float damage;
+	private float distTravelled;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (distTravelled >= range)
+			//Destroy (gameObject);
+	
+	}
+
+	void OnTriggerEnter(Collider obj)
+	{
+		if (obj.tag == "Player") {
+			//deal damage
+		}
+	}
+}
