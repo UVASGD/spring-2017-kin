312daea5f51faa83af46d463d9bf71dcf9150efa
Ryan McCampbell <rmccampbell7@gmail.com>
2016-09-24 14:52:36 -0400

    sfajlkj

--- Kin/Assets/Scripts/TempGameController.cs
+++ Kin/Assets/Scripts/TempGameController.cs
@@ -4,16 +4,20 @@ using System.Collections;
 
 public class TempGameController : MonoBehaviour {
 
-	public GameObject SaveCont;
-	public Canvas canv;
+    public Canvas canv;
+    private SaveController save = SaveController.s_instance;
+    private UIController ui;
 
 	// Use this for initialization
 	void Start () {
-	
+        ui = canv.GetComponent<UIController>();
+        ui.setMaxHealth((int)save.health);
+        ui.setMaxStamina((int)save.stamina);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        ui.setHealth((int)save.health);
+        ui.setStamina((int)save.stamina);
 	}
 }
