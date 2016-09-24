dcd80875140f9ea84b2ae2d9a8127c5b5cecc33b
ferociouself <jhe3cd@virginia.edu>
2016-09-24 14:30:12 -0400

    Added Temp Controller
    Temporary controller to control UI

---
+++ Kin/Assets/Scripts/TempGameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TempGameController : MonoBehaviour {
+
+	public GameObject SaveCont;
+	public Canvas canv;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
