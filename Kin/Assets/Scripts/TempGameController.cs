221a5b704074010000dc68873aef32c1559bc9ee
Ryan McCampbell <rmccampbell7@gmail.com>
2016-10-08 16:03:45 -0400

    new and resume scene

--- Kin/Assets/Scripts/TempGameController.cs
+++ Kin/Assets/Scripts/TempGameController.cs
@@ -5,21 +5,20 @@ using System.Collections;
 public class TempGameController : MonoBehaviour {
 
     public Canvas canv;
-	public GameObject save;
-    private SaveController saveCont;
+    private SaveController save;
     private UIController ui;
 
 	// Use this for initialization
 	void Start () {
-		saveCont = save.GetComponent<SaveController>();
+		save = SaveController.s_instance;
         ui = canv.GetComponent<UIController>();
-        ui.setMaxHealth((int)saveCont.health);
-        ui.setMaxStamina((int)saveCont.stamina);
+        ui.setMaxHealth((int)save.health);
+        ui.setMaxStamina((int)save.stamina);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ui.setHealth((int)saveCont.health);
-        ui.setStamina((int)saveCont.stamina);
+        ui.setHealth((int)save.health);
+        ui.setStamina((int)save.stamina);
 	}
 }
