37073e9abdf277bff681fd28335978defd48fe03
Julian <jaykmcclinton@gmail.com>
2016-10-09 14:09:59 -0400

    Anim Control Script
    Moved code that controlled animator parameters (moving, dead,
    facingRight) to a new script so that minions and stuff can be animated
    properly without being controlled

--- Kin/Assets/Scripts/AvatarMvmController.cs
+++ Kin/Assets/Scripts/AvatarMvmController.cs
@@ -19,32 +19,14 @@ public class AvatarMvmController : MonoBehaviour {
     void Update()
 	{
 		Animator animator = gameObject.GetComponent<Animator> ();
-
-		// direction and animation controls should be moved to new script
-		float angle = 360*Mathf.Atan2(lastMove.x,lastMove.y)/(2*Mathf.PI);
-		// simplify angle to an integer from 0 to 2
-		int direction = 0; bool facingRight = true;
-		if (angle >= -45 && angle < 45)
-			direction = 2;  // facing up
-		else if ((angle > 45 && angle <= 135) || ((angle < -45 && angle >= -135))) 
-		{
-			direction = 1;
-			if (angle < 0) // facing side
-				facingRight = false;
-		}
-		gameObject.GetComponent<SpriteRenderer> ().flipX = !facingRight;
-
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		animator.SetBool ("Dead", animator.GetCurrentAnimatorStateInfo (0).IsTag ("Dead"));
-		animator.SetBool("Moving", move.magnitude > 0);
-		animator.SetFloat("Direction", direction);
 		if(!animator.GetBool("Dead")) rb.velocity = ((Vector2) move) * speed;
 
         // Save Vector2 of last movement
-        if (!(System.Math.Abs(move.x) < 0.01f && System.Math.Abs(move.y) < 0.01f))
-        {
-            lastMove = move;
-        }
+        //if (!(System.Math.Abs(move.x) < 0.01f && System.Math.Abs(move.y) < 0.01f))
+        //{
+        //    lastMove = move;
+        //}
     }
 
     public void playWalkSound()
