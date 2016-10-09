5817f607ff418ed5e82b32fd265b0a721b99c6fc
Julian <jaykmcclinton@gmail.com>
2016-10-09 13:39:57 -0400

    Script Tweaks
    added:
    - link to animation conversion tool :+1:
    
    changes:
    - Sprite is flipped based off direction. Animation controllers modified
    to reflect changes.
    - Sounds are no longer linked to player animation (No clue how to do
    that).
    - Updated Prefabs.

--- Kin/Assets/Scripts/AvatarMvmController.cs
+++ Kin/Assets/Scripts/AvatarMvmController.cs
@@ -18,26 +18,26 @@ public class AvatarMvmController : MonoBehaviour {
 
     void Update()
 	{
-		float angle = 360*Mathf.Atan2(lastMove.x,lastMove.y)/(2*Mathf.PI);
 		Animator animator = gameObject.GetComponent<Animator> ();
 
+		// direction and animation controls should be moved to new script
+		float angle = 360*Mathf.Atan2(lastMove.x,lastMove.y)/(2*Mathf.PI);
+		// simplify angle to an integer from 0 to 2
 		int direction = 0; bool facingRight = true;
-		if (angle >= -45 && angle <= 45)
-			direction = 2;
-		else if (Mathf.Abs (angle) > 45 && Mathf.Abs (angle) <= 135) 
+		if (angle >= -45 && angle < 45)
+			direction = 2;  // facing up
+		else if ((angle > 45 && angle <= 135) || ((angle < -45 && angle >= -135))) 
 		{
 			direction = 1;
-			if (angle < 0)
+			if (angle < 0) // facing side
 				facingRight = false;
 		}
-
+		gameObject.GetComponent<SpriteRenderer> ().flipX = !facingRight;
 
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		animator.SetBool ("Dead", animator.GetCurrentAnimatorStateInfo (0).IsTag ("Dead"));
 		animator.SetBool("Moving", move.magnitude > 0);
-		animator.SetBool("FacingRight", facingRight);
 		animator.SetFloat("Direction", direction);
-        //transform.position += move * speed * Time.deltaTime;
 		if(!animator.GetBool("Dead")) rb.velocity = ((Vector2) move) * speed;
 
         // Save Vector2 of last movement
