7b1c3c07c4ff105626c489eb186bb7dd0829bdd0
Julian <jaykmcclinton@gmail.com>
2016-10-09 12:27:14 -0400

    Anim Controll testing
    compile error fixes from last update from master. Transition between
    directions is controlled by a direction angle

--- Kin/Assets/Scripts/AvatarMvmController.cs
+++ Kin/Assets/Scripts/AvatarMvmController.cs
@@ -17,14 +17,28 @@ public class AvatarMvmController : MonoBehaviour {
     }
 
     void Update()
-    {
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		gameObject.GetComponent<Animator>().SetBool("Moving", move.magnitude > 0);
-		gameObject.GetComponent<Animator>().SetBool("FacingRight", move.x >= 0);
-		gameObject.GetComponent<Animator>().SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-		gameObject.GetComponent<Animator>().SetFloat("Vertical", Input.GetAxis("Vertical"));
+	{
+		float angle = 360*Mathf.Atan2(lastMove.x,lastMove.y)/(2*Mathf.PI);
+		Animator animator = gameObject.GetComponent<Animator> ();
+
+		int direction = 0; bool facingRight = true;
+		if (angle >= -45 && angle <= 45)
+			direction = 2;
+		else if (Mathf.Abs (angle) > 45 && Mathf.Abs (angle) <= 135) 
+		{
+			direction = 1;
+			if (angle < 0)
+				facingRight = false;
+		}
+
+
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		animator.SetBool ("Dead", animator.GetCurrentAnimatorStateInfo (0).IsTag ("Dead"));
+		animator.SetBool("Moving", move.magnitude > 0);
+		animator.SetBool("FacingRight", facingRight);
+		animator.SetFloat("Direction", direction);
         //transform.position += move * speed * Time.deltaTime;
-		rb.velocity = ((Vector2) move) * speed;
+		if(!animator.GetBool("Dead")) rb.velocity = ((Vector2) move) * speed;
 
         // Save Vector2 of last movement
         if (!(System.Math.Abs(move.x) < 0.01f && System.Math.Abs(move.y) < 0.01f))
