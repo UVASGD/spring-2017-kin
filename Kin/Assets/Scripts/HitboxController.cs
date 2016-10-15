db8f865437aa433e309f1272322c048f1be9451b
Reid Bixler <rmb3yz@virginia.edu>
2016-10-15 16:03:47 -0400

    Animation Hitboxes!!!
    Hitboxes for Walk Up, Walk Down, Idle Up, and Idle Down!

--- Kin/Assets/Scripts/HitboxController.cs
+++ Kin/Assets/Scripts/HitboxController.cs
@@ -15,6 +15,29 @@ public class HitboxController : MonoBehaviour {
 	public PolygonCollider2D Character_4;
 	public PolygonCollider2D Character_5;
 
+	//Walk_Down Sprites
+	public PolygonCollider2D Character_9;
+	public PolygonCollider2D Character_10;
+	public PolygonCollider2D Character_11;
+	public PolygonCollider2D Character_12;
+	public PolygonCollider2D Character_13;
+	public PolygonCollider2D Character_14;
+
+	//Idle_Up Sprites
+	public PolygonCollider2D Character_29;
+	public PolygonCollider2D Character_30;
+	public PolygonCollider2D Character_31;
+	public PolygonCollider2D Character_32;
+	public PolygonCollider2D Character_33;
+	public PolygonCollider2D Character_34;
+
+	//Walk_Up Sprites
+	public PolygonCollider2D Character_38;
+	public PolygonCollider2D Character_39;
+	public PolygonCollider2D Character_40;
+	public PolygonCollider2D Character_41;
+	public PolygonCollider2D Character_42;
+	public PolygonCollider2D Character_43;
 
 	//Array of colliders for animation to switch from
 	private PolygonCollider2D[] allHitboxes;
@@ -30,13 +53,35 @@ public class HitboxController : MonoBehaviour {
 		Character_3,
 		Character_4,
 		Character_5,
+		Character_9,
+		Character_10,
+		Character_11,
+		Character_12,
+		Character_13,
+		Character_14,
+		Character_29,
+		Character_30,
+		Character_31,
+		Character_32,
+		Character_33,
+		Character_34,
+		Character_38,
+		Character_39,
+		Character_40,
+		Character_41,
+		Character_42,
+		Character_43,
 		clear // special case to remove all boxes
 	}
 
 
 	// Use this for initialization
 	void Start () {
-		allHitboxes = new PolygonCollider2D[]{Character_0,Character_1,Character_2,Character_3,Character_4,Character_5};
+		allHitboxes = new PolygonCollider2D[]{
+			Character_0,Character_1,Character_2,Character_3,Character_4,Character_5,
+			Character_9,Character_10,Character_11,Character_12,Character_13,Character_14,
+			Character_29,Character_30,Character_31,Character_32,Character_33,Character_34,
+			Character_38,Character_39,Character_40,Character_41,Character_42,Character_43};
 
 		//Create the collider
 		curHitbox = gameObject.AddComponent<PolygonCollider2D>();
