21bad72c7f4222d426617693462cacdf49cd8152
Reid Bixler <rmb3yz@virginia.edu>
2016-10-22 15:13:24 -0400

    Added LevelUpController
    Kathy’s LevelUpController Integrate

---
+++ Kin/Assets/Scripts/LevelUpController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelUpController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//getters
+	int getPlayerLevel()
+	{
+		return playerLevel;
+	}
+
+	int getOrderLevel()
+	{
+		return orderLevel;
+	}
+
+	//setters
+	void setPlayerLevel(int inputPlayerLevel)
+	{
+		playerLevel = inputPlayerLevel;
+	}
+
+	void setOrderLevel(int inputOrderLevel)
+	{
+		orderLevel = inputOrderLevel;
+	}
+
+
+	//level up
+	void playerLevelUp()
+	{
+		playerLevel++;
+	}
+
+	void orderLeveUp()
+	{
+		orderLevel++;
+	}
+}
