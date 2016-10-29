1469fb813d9250735404bd8760e60d09091e1fc9
Jared Capelle <jjc2bd@virginia.edu>
2016-10-29 15:54:16 -0400

    Created Stat Screen Controller
    plus weird sln change...

---
+++ Kin/Assets/Scripts/UIScripts/StatScreenController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatScreenController : MonoBehaviour {
+	// Use this for initialization
+	void Awake () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
