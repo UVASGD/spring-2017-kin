395d86ee4c52beccd1a531c5d810784ee65aa025
Soupdoop <jherd23@gmail.com>
2016-11-04 19:05:54 -0400

    Basic Stuff for Anim Controller

--- Kin/Assets/Scripts/MeleeMinionAnimationController.cs
+++ Kin/Assets/Scripts/MeleeMinionAnimationController.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+[RequireComponent (typeof(SpriteRenderer))]
+[RequireComponent (typeof(Rigidbody2D))]
 public class MeleeMinionAnimationController : MonoBehaviour
 {
+
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+	public Vector2 lastMove;
+	Animator animator;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+		lastMove = new Vector2 (0, 0);
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
