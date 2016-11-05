5ae88c61e4b5bfd606743e668510bb5db66915d7
zsd4yr <zsd4yr@virginia.edu>
2016-11-04 21:32:05 -0400

    player fx

---
+++ Kin/Assets/Scripts/Player Scripts/PlaySound.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	public string name;
+	public AudioSource aud;
+	private AudioClip right_dirt;
+	private AudioClip left_dirt;
+	private AudioClip right_grass;
+	private AudioClip left_grass;
+
+	// Use this for initialization
+	void Start () {
+		right_dirt = Resources.Load ("Sounds/Player_FX/Footsteps_Dirt_Right") as AudioClip;
+		left_dirt = Resources.Load("Sounds/Player_FX/Footsteps_Dirt_Left") as AudioClip;
+		right_grass = Resources.Load("Sounds/Player_FX/Footsteps_Grass_Right") as AudioClip;
+		left_grass = Resources.Load("Sounds/Player_FX/Footsteps_Grass_Left") as AudioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void playSound(){
+		aud.Play ();
+	}
+
+	public void playRightGrass(){
+		aud.clip = right_dirt;
+		playSound ();
+	}
+
+	public void playLeftGrass(){
+		aud.clip = left_dirt;
+		playSound ();
+	}
+
+	public void playRightDirt(){
+		aud.clip = right_dirt;
+		playSound ();
+	}
+
+	public void playLeftDirt(){
+		aud.clip = left_dirt;
+		playSound ();
+	}
+
+
+}
