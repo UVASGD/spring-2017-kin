2c4bb52d7e52849c8bdcb38a99fb52ea573b2ddb
zsd4yr <zsd4yr@virginia.edu>
2016-11-05 17:24:37 -0400

    yep

--- Kin/Assets/Scripts/AIScripts/BaseGodAI.cs
+++ Kin/Assets/Scripts/AIScripts/BaseGodAI.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEditor;
+//using UnityEditor;
 using System.Collections;
 
 public class BaseGodAI : MonoBehaviour {
--- Kin/Assets/Scripts/AIScripts/MinionScripts/EnemyHealth.cs
+++ Kin/Assets/Scripts/AIScripts/MinionScripts/EnemyHealth.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEditor;
+//using UnityEditor;
 using System.Collections;
 
 public class EnemyHealth:MonoBehaviour
