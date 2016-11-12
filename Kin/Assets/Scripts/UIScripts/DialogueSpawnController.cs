bf7d80bda1d216dbb6815ea83633de2705b1ef3d
ferociouself <jhe3cd@virginia.edu>
2016-11-12 15:31:58 -0500

    XML Parsing and Dialogue Testing

---
+++ Kin/Assets/Scripts/UIScripts/DialogueSpawnController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueSpawnController : MonoBehaviour {
+
+	public float writeSpeed;
+
+	string dialogueString;
+
+	public enum PersonType {
+
+	}
+
+	public PersonType person;
+
+	DialogueXMLParser parser;
+	public GameObject xmlParser;
+
+	bool interacted = false;
+
+	public GameObject speaker;
+
+	// Use this for initialization
+	void Start () {
+		parser = xmlParser.GetComponent<DialogueXMLParser>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			Debug.Log(parser.RequestDialogue("Wisdom Trainer", "Init"));
+		}
+	}
+}
