19c5658b14740627ce9444d2524eed15779a392a
gtg3vv <gtg3vv@virginia.edu>
2016-11-12 16:10:11 -0500

    Chac ranged

--- Kin/Assets/Scripts/AIScripts/GodScripts/ChacTrad.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/ChacTrad.cs
@@ -1,33 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ChacTrad : MonoBehaviour {
+public class ChacTrad : BaseGodAI {
 
-	float fireBoltCd;
-	float accuracy;
-	float boltSpeed;
+	float fireBoltCd = 0;
+	bool boltOnCd = false;
+	float accuracy = 100;
+	float boltSpeed = 2.0f;
+	const float ANGLE_THRESHOLD = Mathf.PI/4;
 	// Use this for initialization
-	void Start () {
-	
+	protected override void Start () {
+		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	protected override void Update () {
+		if (!boltOnCd) {
+			fireBolt (2);
+			boltOnCd = true;
+			fireBoltCd = 1.0f;
+		} else {
+			fireBoltCd -= Time.deltaTime;
+			if (fireBoltCd < 0.0f)
+				boltOnCd = false;
+		}
 	
 	}
 
 	void fireBolt(int type){
+		GameObject newProj1, newProj2, newProj3, newProj4, newProj5, newProj6;
+		float angle = predictLocation();
 		switch (type) {
 		case 1:
-			float angle = predictLocation();
 			//Instantiate projectile from prefab Instantiate(prefab,minionposition,no rotation)
-			GameObject newProj1 = (GameObject) GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
-			newProj1.GetComponent<Rigidbody2D> ().velocity = new Vector2(boltSpeed*Mathf.Cos(angle),boltSpeed*Mathf.Sin(angle));
-			GameObject newProj2 = (GameObject) GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
-			newProj2.GetComponent<Rigidbody2D> ().velocity = new Vector2(boltSpeed*Mathf.Cos(angle),boltSpeed*Mathf.Sin(angle));
-			GameObject newProj3 = (GameObject) GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
-			newProj3.GetComponent<Rigidbody2D> ().velocity = new Vector2(boltSpeed*Mathf.Cos(angle),boltSpeed*Mathf.Sin(angle));
-		}
+			newProj1 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle-.5f), boltSpeed * Mathf.Sin (angle-.5f));
+			newProj2 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle), boltSpeed * Mathf.Sin (angle));
+			newProj3 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj3.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle+.5f), boltSpeed * Mathf.Sin (angle+.5f));
+			break;
+		case 2:
+			newProj1 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle), boltSpeed * Mathf.Sin (angle));
+			break;
+		case 3:
+			newProj1 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle - 1.0f), boltSpeed * Mathf.Sin (angle - 1.0f));
+			newProj2 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle - .5f), boltSpeed * Mathf.Sin (angle - .5f));
+			newProj3 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj3.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle), boltSpeed * Mathf.Sin (angle));
+			newProj4 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj4.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle + .5f), boltSpeed * Mathf.Sin (angle + .5f));
+			newProj5 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj5.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle + 1.0f), boltSpeed * Mathf.Sin (angle + 1.0f));
+			newProj6 = (GameObject)GameObject.Instantiate (Resources.Load ("Prefabs/MinionProj", typeof(GameObject)), gameObject.transform.position, Quaternion.identity);
+			newProj6.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boltSpeed * Mathf.Cos (angle + 1.5f), boltSpeed * Mathf.Sin (angle + 1.5f));
+			break;
+			
+		default:
+			return;
+		} 
 		
 	}
 
@@ -46,7 +80,7 @@ public class ChacTrad : MonoBehaviour {
 	protected float predictLocation(){
 		float C2 = (gameObject.transform.position.x-targetObject.transform.position.x);
 		float C3 = (targetObject.transform.position.y-gameObject.transform.position.y);
-		float C1 = ((targetObject.GetComponent<Rigidbody2D> ().velocity.y*C2 + targetObject.GetComponent<Rigidbody2D> ().velocity.x*C3)/projectileSpeed);
+		float C1 = ((targetObject.GetComponent<Rigidbody2D> ().velocity.y*C2 + targetObject.GetComponent<Rigidbody2D> ().velocity.x*C3)/boltSpeed);
 		float leading = 2*Mathf.Atan((C2+Mathf.Sqrt(-1*(C1*C1)+C2*C2+C3*C3))/(C1+C3));
 		float still = Mathf.Atan2(C3,-C2);
 		float difference = leading-still;
