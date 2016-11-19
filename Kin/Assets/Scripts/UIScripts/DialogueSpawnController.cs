83e8bb8730e4679723312357617fe0950af1ff18
ferociouself <jhe3cd@virginia.edu>
2016-11-19 14:25:40 -0500

    Fixed the bois

--- Kin/Assets/Scripts/UIScripts/DialogueSpawnController.cs
+++ Kin/Assets/Scripts/UIScripts/DialogueSpawnController.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DialogueSpawnController : MonoBehaviour {
 
 	public float writeSpeed;
+	public int periodMult;
+
+	public GameObject diaTextBox;
+
+	float writeTimer;
+	int curLength = 0;
 
 	string dialogueString;
+	string curDiaStr;
 
 	public enum PersonType {
 
@@ -28,7 +36,28 @@ public class DialogueSpawnController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Y)) {
-			Debug.Log(parser.RequestDialogue("WisdomTrainer", "Init", 0));
+			dialogueString = parser.RequestDialogue("HealthTrainer", "Init", 0);
+			Debug.Log(dialogueString);
 		}
+		if (dialogueString != null && dialogueString.Length > 0) {
+			if (writeTimer < writeSpeed) {
+				writeTimer += Time.deltaTime;
+			} else if (curLength < dialogueString.Length) {
+				curLength++;
+				curDiaStr = dialogueString.Substring(0, curLength);
+				writeTimer = (curDiaStr.EndsWith(".") ? periodMult * -writeSpeed : 0);
+				diaTextBox.GetComponent<Text>().text = curDiaStr;
+			}
+			if (Input.GetKeyDown(KeyCode.U)) {
+				curLength = dialogueString.Length;
+				curDiaStr = dialogueString.Substring(0, curLength);
+				diaTextBox.GetComponent<Text>().text = curDiaStr;
+				writeTimer = writeSpeed;
+			}
+		}
+	}
+
+	public string GetCurrentString() {
+		return curDiaStr;
 	}
 }
