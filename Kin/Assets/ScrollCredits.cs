e562d6bca4fcaf711eef8a993f1525bcd2ff79c9
Ryan McCampbell <rmccampbell7@gmail.com>
2016-12-03 16:12:31 -0500

    scrolling credits

---
+++ Kin/Assets/ScrollCredits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollCredits : MonoBehaviour {
+
+    public float speed = 1;
+    private RectTransform rTrans;
+    private float maxY;
+
+	// Use this for initialization
+	void Start () {
+        rTrans = GetComponent<RectTransform>();
+        RectTransform pTrans = transform.parent.GetComponent<RectTransform>();
+        maxY = pTrans.rect.yMax;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (rTrans.rect.yMax < maxY)
+        {
+            transform.position = transform.position + Vector3.up * speed;
+        }
+	}
+}
