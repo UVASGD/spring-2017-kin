7c8f6fe03174e12bee6d2325108c09dd649f4ead
ferociouself <jhe3cd@virginia.edu>
2016-12-06 21:44:27 -0500

    Added Death Scenes and Death Scripts

---
+++ Kin/Assets/Scripts/UIScripts/DeathMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeathMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ResumeGame() {
+		SceneManager.LoadScene("Main");
+	}
+
+	public void MainMenu() {
+		SceneManager.LoadScene("Main Menu");
+	}
+}
