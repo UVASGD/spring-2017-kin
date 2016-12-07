4cf25c7e58618e662b28e9d73f082ece0590ca50
ferociouself <jhe3cd@virginia.edu>
2016-12-06 21:44:27 -0500

    Added Death Scenes and Death Scripts

---
+++ Kin/Assets/Scripts/CoreScripts/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public IEnumerator FadeToDeath() {
+		float fadeTime = gameObject.GetComponent<Faded>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene("Death");
+	}
+
+	public IEnumerator FadeFromDeath() {
+		float fadeTime = gameObject.GetComponent<Faded>().BeginFade(-1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene("Main");
+	}
+}
