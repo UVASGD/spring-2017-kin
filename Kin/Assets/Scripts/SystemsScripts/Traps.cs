63f56ded68cb98ad36349fab11ccc50acd0a4f6d
sg2nq <Siddhant@d-172-25-208-200.dhcp.virginia.edu>
2017-02-18 13:54:18 -0500

    Traps
    Raps

--- Kin/Assets/Scripts/SystemsScripts/Traps.cs
+++ Kin/Assets/Scripts/SystemsScripts/Traps.cs
@@ -4,35 +4,41 @@ using UnityEngine;
 
 public class Traps : MonoBehaviour {
 
-    BoxCollider2D collider;
     int damage;
 
 	// Use this for initialization
 	void Start () {
-        collider = gameObject.GetComponent<BoxCollider2D>();
+
+//        collider = gameObject.GetComponent<BoxCollider2D>();
         damage = 10;
+//
+//		collider = gameObject.GetComponent<BoxCollider2D>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Boom, Trap expoded.");
-        GameObject target = other.gameObject;
-        PlayerHealth pHealth;
-        EnemyHealth eHealth;
-        if(target.tag == "Player")
-        {
-            pHealth = target.GetComponent<PlayerHealth>();
-            pHealth.TakeDamage(damage);
-        }
-        else if(target.tag == "enemy") {
-            eHealth = target.GetComponent<EnemyHealth>();
-            eHealth.takeDamage(damage);
-        }
 
-        
-    }
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("Boom, Trap expoded.");
+
+		GameObject target = other.gameObject;
+		PlayerHealth pHealth;
+		EnemyHealth eHealth;
+		if (target.tag == "Player") {
+			pHealth = target.GetComponent<PlayerHealth> ();
+			pHealth.TakeDamage (damage);
+		} else if (target.tag == "enemy") {
+			eHealth = target.GetComponent<EnemyHealth> ();
+			eHealth.takeDamage (damage);
+		}
+//		GameObject part = (GameObject)(Resources.Load ("Prefabs/XPParticles", typeof(GameObject)));
+//		GameObject instPart = Instantiate (part, transform.position, Quaternion.identity);
+//		instPart.GetComponent<ParticleEmit> ().UpdateParticles ();
+		Destroy (gameObject);
+
+	}
 }
