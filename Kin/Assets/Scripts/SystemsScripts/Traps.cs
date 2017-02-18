685d232d9c652fc57c4b31694bb2dac3cdc45377
Jared Capelle <jjc2bd@virginia.edu>
2017-02-18 13:33:13 -0500

    Traps do damage and stuff

--- Kin/Assets/Scripts/SystemsScripts/Traps.cs
+++ Kin/Assets/Scripts/SystemsScripts/Traps.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Traps : MonoBehaviour {
 
     BoxCollider2D collider;
+    int damage;
 
 	// Use this for initialization
 	void Start () {
         collider = gameObject.GetComponent<BoxCollider2D>();
+        damage = 10;
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,19 @@ public class Traps : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Boom, Trap expoded.");
+        GameObject target = other.gameObject;
+        PlayerHealth pHealth;
+        EnemyHealth eHealth;
+        if(target.tag == "Player")
+        {
+            pHealth = target.GetComponent<PlayerHealth>();
+            pHealth.TakeDamage(damage);
+        }
+        else if(target.tag == "enemy") {
+            eHealth = target.GetComponent<EnemyHealth>();
+            eHealth.takeDamage(damage);
+        }
+
+        
     }
 }
