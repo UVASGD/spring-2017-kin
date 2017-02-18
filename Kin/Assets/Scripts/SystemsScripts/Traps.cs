bed127b93c2cd4996d89839d50e1f253b7ede10f
Jared Capelle <jjc2bd@virginia.edu>
2017-02-18 12:53:10 -0500

    Made a testing scene and stuff

--- Kin/Assets/Scripts/SystemsScripts/Traps.cs
+++ Kin/Assets/Scripts/SystemsScripts/Traps.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Traps : MonoBehaviour {
 
+    BoxCollider2D collider;
+
 	// Use this for initialization
 	void Start () {
-		
+        collider = gameObject.GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter(Collider thing)
+    {
+
+        print("HELP I WAS STEPPED ON");
+    }
 }
