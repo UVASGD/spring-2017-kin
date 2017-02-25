0e6faac5dd804f10b2f2d177f708cc9cf8f7cc5c
lauramaimon <lcm4fe@virginia.edu>
2017-02-25 12:39:05 -0500

    Special stair code!
    Stairs have trigger with tag dr_stair or dl_stair. When you're on
    dr_stair, right = down/right, left = up/left, etc.
    Changed animation controller so you're animated as moving sideways when
    you're moving at a 45 degree angle

--- Kin/Assets/Scripts/PlayerScripts/AvatarMvmController.cs
+++ Kin/Assets/Scripts/PlayerScripts/AvatarMvmController.cs
@@ -1,31 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AvatarMvmController : MonoBehaviour {
+public class AvatarMvmController : MonoBehaviour
+{
 
     public float speed;
+    public bool dr_stairs = false;
+    public bool dl_stairs = false;
 
-	Rigidbody2D rb;
+    Rigidbody2D rb;
 
     public Vector2 lastMove = new Vector2(0, 0);
 
     void Start()
     {
-		rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
-	{
-		Animator animator = gameObject.GetComponent<Animator> ();
-		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		if(!animator.GetBool("Dead")) rb.velocity = ((Vector2) move.normalized) * speed;
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+        
+        if (!animator.GetBool("Dead"))
+        {
+            if (!dr_stairs && !dl_stairs)
+            {
+                var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+                rb.velocity = ((Vector2)move.normalized) * speed;
+            }
+            else if (!dr_stairs && dl_stairs)
+            {
+                //right = up/right
+                //left = down/left
+                var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") + Input.GetAxis("Horizontal"), 0);
+                rb.velocity = ((Vector2)move.normalized) * speed;
+            }
+            else if (dr_stairs && !dl_stairs)
+            {
+                //right = down/right
+                //left = up/left
+                var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") - Input.GetAxis("Horizontal"), 0);
+                rb.velocity = ((Vector2)move.normalized) * speed;
+            }
+            else
+            {
+                // shouldn't happen, move normally
+                var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+                rb.velocity = ((Vector2)move.normalized) * speed;
+            }
+        }
+        lastMove = gameObject.GetComponent<AnimationControl>().lastMove;
+    }
 
-		lastMove = gameObject.GetComponent<AnimationControl>().lastMove;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "dr_stair")
+        {
+            dr_stairs = true;
+        }
+        if (other.gameObject.tag == "dl_stair")
+        {
+            dl_stairs = true;
+        }
+    }
 
-        // Save Vector2 of last movement
-        //if (!(System.Math.Abs(move.x) < 0.01f && System.Math.Abs(move.y) < 0.01f))
-        //{
-        //    lastMove = move;
-        //}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "dr_stair")
+        {
+            dr_stairs = false;
+        }
+        if (other.gameObject.tag == "dl_stair")
+        {
+            dl_stairs = false;
+        }
     }
 }
