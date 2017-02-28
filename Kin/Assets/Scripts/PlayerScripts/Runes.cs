33192d2d427049608e8ef298b826afffe4a2406e
Abey Koolipurackal <abey@virginia.edu>
2017-02-28 17:45:31 -0500

    Created Runes Script

---
+++ Kin/Assets/Scripts/PlayerScripts/Runes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runes : MonoBehaviour {
+
+	public bool ixtabRuneActivated;
+	public bool mazeRuneActivated;
+	public bool moonRuneActivated;
+	public bool chacRuneActivated;
+	public bool huntRuneActivated;
+	public bool mirrorjaguarRuneActivated;
+	public bool sunRuneActivated;
+	public bool twinsRuneActivated;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
