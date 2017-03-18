27ede0371b23c81caa677c126255929c24c8517e
Hasan Khan <theotherkhan@gmail.com>
2017-03-18 14:00:46 -0400

    Chac State Machine Commit 1
    Chac State Machine Commit 1

---
+++ Kin/Assets/Scripts/AIScripts/GodScripts/ChacLocation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChacLocation : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
