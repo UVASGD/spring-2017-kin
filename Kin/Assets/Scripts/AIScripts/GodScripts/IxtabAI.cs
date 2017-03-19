1fbb83f86806cea993151d1559f73fad4481a173
Zachary Danz <zsd4yr@virginia.edu>
2017-03-19 13:28:17 -0400

    closer

--- Kin/Assets/Scripts/AIScripts/GodScripts/IxtabAI.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/IxtabAI.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent (typeof(EnemyHealth), (typeof(Animator)), (typeof(Rigidbody2D)))]
 public class IxtabAI : MonoBehaviour, BaseAI {
 
 	public GameObject player;
-	public GameObject UI;
+	public Slider UIHealth;
+	public Text UIName;
 	public float detectRange;
 	public float speed;
 	private EnemyHealth enemyHealth;
@@ -14,12 +16,20 @@ public class IxtabAI : MonoBehaviour, BaseAI {
 	private Rigidbody2D rb2;
 	public bool calm;
 
+	private float slamCooldown;
+	private float swipeCooldown;
+	private float invisCooldown;
+
 	// Use this for initialization
 	void Start () {
 		enemyHealth = this.gameObject.GetComponent<EnemyHealth> ();
 		anim = this.gameObject.GetComponent<Animator> ();
 		rb2 = this.gameObject.GetComponent<Rigidbody2D> ();
 		calm = true;
+
+		slamCooldown = 0.0f;
+		swipeCooldown = 0.0f;
+		invisCooldown = 0.0f;
 	}
 	
 	// Update is called once per frame
@@ -29,16 +39,32 @@ public class IxtabAI : MonoBehaviour, BaseAI {
 		Debug.Log ("Distance: "+dfp);
 		anim.SetFloat("DistanceFromPlayer", dfp);
 
-		if (calm && dfp <= detectRange) {
+		slamCooldown -= Time.deltaTime;
+		swipeCooldown -= Time.deltaTime;
+		invisCooldown -= Time.deltaTime;
+
+		if (calm && dfp <= detectRange) { //activate!
 			calm = false;
+			UIHealth.enabled = true;
+			UIName.enabled = true;
 		}
 
 
 		if (!calm) {
-			if (dfp > detectRange + 1) {
+			if (dfp > detectRange + 0.5) { //deactivate!
 				calm = true;
-			} else if (dfp > 0.5) {
+				UIHealth.enabled = false;
+				UIName.enabled = false;
+			} else if (dfp > 0.8) { //active & approaching
 				rb2.velocity = ((Vector2)ptp - (Vector2)this.gameObject.transform.position).normalized * speed;
+			} else { //active and within range
+				rb2.velocity = Vector2.zero;
+
+				// attack logic with cooldown resets
+
+
+
+
 			}
 		} else {
 			rb2.velocity = Vector2.zero;
