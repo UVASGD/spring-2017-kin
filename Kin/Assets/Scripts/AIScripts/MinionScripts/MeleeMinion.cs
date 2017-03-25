cab1dbfb5ddddec3b90f70957b367c676224e39c
Zachary Danz <zsd4yr@virginia.edu>
2017-03-25 14:20:02 -0400

    exp summary

--- Kin/Assets/Scripts/AIScripts/MinionScripts/MeleeMinion.cs
+++ Kin/Assets/Scripts/AIScripts/MinionScripts/MeleeMinion.cs
@@ -94,6 +94,7 @@ public class MeleeMinion : BaseMinionAI
     }
 
 	public void PlaySoundPlz(){
+		
 		GetComponent<AudioSource>().Play();
 	}
 
--- Kin/Assets/Scripts/PlayerScripts/PlayerExperience.cs
+++ Kin/Assets/Scripts/PlayerScripts/PlayerExperience.cs
@@ -26,6 +26,10 @@ public class PlayerExperience : MonoBehaviour
         }
     }
 
+	void OnGUI(){
+		GUI.Button(new Rect (Screen.width - 120, 50, 100, 26), ("EXP: "+currentExp.ToString()));
+	}
+
     public void setCurrentExp(long exp)
     {
         this.currentExp = exp;
