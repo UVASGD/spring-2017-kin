4863d3e6f805f408fb93915906765625bf18b23d
Zachary Danz <zsd4yr@virginia.edu>
2017-03-25 10:59:16 -0400

    ixtab music and monastery in main

---
+++ Kin/Assets/IxtabMusicTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IxtabMusicTrigger : MonoBehaviour {
+
+	public GameObject DNH;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D coll){
+		if (coll.tag == "Player" && DNH.GetComponent<MusicController>().state != MusicController.MusicState.Boss) {
+			DNH.GetComponent<MusicController> ().InterruptForBoss ("Ixtab");
+		}
+	}
+
+	public void OnTiggerExit2D(Collider2D coll){
+		if (coll.tag == "Player" && DNH.GetComponent<MusicController>().state != MusicController.MusicState.World) {
+			DNH.GetComponent<MusicController> ().InterruptForWorld ();
+		}
+	}
+}
