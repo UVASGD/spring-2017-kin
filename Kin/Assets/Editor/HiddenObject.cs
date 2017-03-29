571a1a998d3e77d44ea51f69e12eb4e60b640b5f
ferociouself <jhe3cd@virginia.edu>
2017-03-29 17:14:37 -0400

    Editor Tools only run in Editor
    Maybe Cloud Build will work now?

--- Kin/Assets/Editor/HiddenObject.cs
+++ Kin/Assets/Editor/HiddenObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
@@ -73,3 +74,4 @@ public class HiddenObjectExplorer : EditorWindow {
         GUILayout.EndScrollView();
     }
 }
+#endif
