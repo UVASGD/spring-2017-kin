aea8d9af6394d724fc6a37f70c5db276ae070bf6
Kenny Chen <kc5by@virginia.edu>
2017-04-08 14:06:15 -0400

    Created SpawnStalagtites, shoot meteor, and spawn snake methods

--- Kin/Assets/Scripts/AIScripts/GodScripts/Quetzalcotl/QuetzalcotlAI.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/Quetzalcotl/QuetzalcotlAI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class QuetzalcotlAI : MonoBehaviour {
 
 	public const float MAX_ATTACK_CD = 5.0f;
-	public const float MAX_STALAG_CD = 10.0f;
+	public const float MAX_STALAG_CD = 5.0f;
 	public const float MAX_SNAKE_CD = 15.0f;
 	public const float MAX_METEOR_CD = 2.5f;
 
@@ -25,7 +25,16 @@ public class QuetzalcotlAI : MonoBehaviour {
 	public float aggroRadius = 4.0f;
 
     public float stageRadius = 4.0f;
-    protected float offset = 0.3f;
+    private float offset = 0.3f;
+    private float stalagSpeed = 5.0f;
+    public GameObject targetObject;
+    private float baseX;
+    private float changeX;
+    private float baseY;
+    private float changeY;
+    private float radius = 1.0f;
+    private int numStalagSpots = 3;
+    private int numStalag = 4;
 
 	public GameObject wallCreator;
 
@@ -33,12 +42,25 @@ public class QuetzalcotlAI : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        CloseArena();		
+        baseX = gameObject.transform.position.x;
+        baseY = gameObject.transform.position.y;
+
+        changeX = 2.0f;
+        changeY = 2.0f;
+        //CloseArena();		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        if (curStalagCD <= 0.0f)
+        {
+            SpawnStalagtites();
+            curStalagCD = MAX_STALAG_CD;
+        }
+        else
+            curStalagCD -= Time.deltaTime;
 	}
 
 	public void CloseArena()
@@ -54,6 +76,7 @@ public class QuetzalcotlAI : MonoBehaviour {
         */
 
         //Create Square Arena
+        //currently, square around boss location, need to change once moved off cliff
         //top wall
         for(int x = 0; x < 14; x++)
         {
@@ -100,17 +123,62 @@ public class QuetzalcotlAI : MonoBehaviour {
         }
     }
 
-	public void SpawnStalagtites() {
+	public void SpawnStalagtites()
+    {
+        for(int x = 0; x < numStalagSpots; x++)
+        {
+            float centerX = Random.Range(gameObject.transform.position.x - changeX, gameObject.transform.position.x + changeX);
+            float centerY = Random.Range(gameObject.transform.position.y, gameObject.transform.position.y + changeY);
 
-	}
+            for(int y = 0; y < numStalag; y++)
+            {
+                float ang = Random.Range(0, 2*Mathf.PI);
+                float rad = Random.Range(0, radius);
+                GameObject proj = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Projectiles/LightningBolt", typeof(GameObject)),
+                    new Vector3(centerX + rad * Mathf.Cos(ang), gameObject.transform.position.y + rad * Mathf.Sin(ang)), Quaternion.identity);
+            }
+        }
+        
+        
+    }
 
-	public void SpawnSnakes() {
+    public void SpawnSnakes()
+    {
+        foreach (Vector2 vec in calculateAngles(8, 0.5f))
+        {
+            GameObject go = Instantiate(Resources.Load("Prefabs/Projectiles/Skullcandy", typeof(GameObject)) as GameObject,
+                vec, Quaternion.identity) as GameObject;
+            go.GetComponent<SkullCandy>().setVelocity(gameObject.transform.position);
+        }
+    }
 
-	}
+    private List<Vector2> calculateAngles(int num, float distanceAway)
+    {
+        if (num == 0)
+        {
+            //return new List<Vector2>();
+            num = 3;
+        }
+        int angleDisplacement = 360 / num;
+        int curAngle = 0;
+        List<Vector2> vecList = new List<Vector2>();
+        for (int i = 0; i < num; i++)
+        {
+            vecList.Add((Vector2)gameObject.transform.position +
+                new Vector2(Mathf.Cos(Mathf.Deg2Rad * curAngle), Mathf.Sin(Mathf.Deg2Rad * curAngle)).normalized * distanceAway);
+            curAngle += angleDisplacement;
+        }
+        return vecList;
+    }
 
-	public void ShootMeteor() {
-
-	}
+    public void ShootMeteor()
+    {
+        float angle = Mathf.Atan2(targetObject.transform.position.y - gameObject.transform.position.y, targetObject.transform.position.x - gameObject.transform.position.x);
+        GameObject proj = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Brain the Hotdog", typeof(GameObject)),
+            new Vector3(gameObject.transform.position.x + 0.1f * Mathf.Cos(angle), gameObject.transform.position.y + 0.1f * Mathf.Sin(angle)), Quaternion.identity);
+        proj.AddComponent<Stalagtites>(); //delete once made prefab
+        proj.GetComponent<Rigidbody2D>().velocity = new Vector2(stalagSpeed * Mathf.Cos(angle), stalagSpeed * Mathf.Sin(angle));
+    }
 
 	public void SwipeFist() {
 
