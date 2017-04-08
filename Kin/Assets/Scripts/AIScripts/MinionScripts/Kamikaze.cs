5331649db7827957862e6d1f81f4d796daa6f186
zsd4yr <zsd4yr@virginia.edu>
2017-04-08 13:10:39 -0400

    quality of life fixes

--- Kin/Assets/Scripts/AIScripts/MinionScripts/Kamikaze.cs
+++ Kin/Assets/Scripts/AIScripts/MinionScripts/Kamikaze.cs
@@ -32,7 +32,7 @@ public class Kamikaze : BaseMinionAI
         timeToExplode = 0.0f;
 		timeSinceDeath = 0.0f;
         explodeDelay = .9f;
-		awarenessRadius = 1.0f; //3.0f;
+		if(awarenessRadius == 0.0f) awarenessRadius = 1.0f; //3.0f;
 		decayTime = 10.0f;
 		exploded = false;
         speed = 1.0f;
