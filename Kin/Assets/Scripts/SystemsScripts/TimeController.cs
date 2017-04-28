24f7e09c96d0664381bb886b520b20b6bc3dfe3f
ferociouself <jhe3cd@virginia.edu>
2017-04-28 12:59:47 -0400

    manual time merging
    whoops my fault

--- Kin/Assets/Scripts/SystemsScripts/TimeController.cs
+++ Kin/Assets/Scripts/SystemsScripts/TimeController.cs
@@ -4,40 +4,43 @@ using System.Collections;
 public class TimeController : MonoBehaviour {
 	public float dayLength = 330.0f;
 
-	private int hour;
-	private int minute;
+	private static int hour;
+	private static int minute;
 
-	private DayNightController DNC;
+	private static DayNightController DNC;
 
-	public GameObject calendar;
+	private static CalendarController calendar;
+
+	private static bool calendarActive = false;
 
 	/// <summary>
 	/// The day.
 	/// </summary>
-	public int kin;
+	public static int kin = 0;
 	/// <summary>
 	/// 20 kin.
 	/// </summary>
-	private int uinal;
+	private static int uinal;
 	/// <summary>
 	/// 18 uinal.
 	/// </summary>
-	private int tun;
+	private static int tun;
 	/// <summary>
 	/// 20 tun.
 	/// </summary>
-	private int katun;
+	private static int katun;
 	/// <summary>
 	/// 20 katun; at 13 the game ends
 	/// </summary>
-	private int baktun;
+	private static int baktun;
 
 	// Use this for initialization
 	void Start () {
-		kin = 0;
-
 		CalculateCalendar ();
 		DNC = this.gameObject.GetComponent<DayNightController> ();
+		DontDestroyOnLoad (this.gameObject);
+		if (GameObject.Find("Calendar")) calendar = GameObject.Find("Calendar").GetComponent<CalendarController>();
+		calendarActive = calendar != null;
 	}
 
 	void Update(){
@@ -57,7 +60,7 @@ public class TimeController : MonoBehaviour {
 	/// Progresses the day.
 	/// </summary>
 	/// <param name="byNum">By number.</param>
-	private void ProgressDay(int byNum) {
+	public static void ProgressDay(int byNum) {
 		kin += Mathf.Abs(byNum);
 		CalculateCalendar ();
 	}
@@ -65,8 +68,10 @@ public class TimeController : MonoBehaviour {
 	/// <summary>
 	/// Calculates the calendar.
 	/// </summary>
-	private void CalculateCalendar(){
-		calendar.GetComponent<CalendarController>().CalendarSet(kin);
+	private static void CalculateCalendar(){
+		if (calendarActive) {
+			calendar.CalendarSet(kin);
+		}
 		uinal = kin % 20;
 		tun = uinal % 18;
 		katun = tun % 20;
