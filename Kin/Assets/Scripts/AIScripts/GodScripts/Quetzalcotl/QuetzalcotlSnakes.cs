89b9fb52ea7620bae7347f2e0fa1e10b4f91e5f9
ferociouself <jhe3cd@virginia.edu>
2017-05-02 20:13:08 -0400

    Some stuff

--- Kin/Assets/Scripts/AIScripts/GodScripts/Quetzalcotl/QuetzalcotlSnakes.cs
+++ Kin/Assets/Scripts/AIScripts/GodScripts/Quetzalcotl/QuetzalcotlSnakes.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class QuetzalcotlSnakes : StateMachineBehaviour {
 
+	QuetzalcotlAI ai;
+
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		ai = animator.transform.parent.gameObject.GetComponent<QuetzalcotlAI>();
+	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -15,9 +17,9 @@ public class QuetzalcotlSnakes : StateMachineBehaviour {
 	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		ai.SpawnSnakes();
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
