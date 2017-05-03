87441cc3df89030a923e724a75db061726e60e12
Zachary Danz <zsd4yr@virginia.edu>
2017-05-02 20:04:53 -0400

    please

--- Kin/Assets/Scripts/UIScripts/DialogueSpawnController.cs
+++ Kin/Assets/Scripts/UIScripts/DialogueSpawnController.cs
@@ -58,7 +58,7 @@ public class DialogueSpawnController : MonoBehaviour {
                 }
                 finished = true;
 			}
-			if (GameObject.FindObjectOfType<InputOverrideController>().IsNormal() && Input.GetKeyDown(KeyCode.Space)) {
+			if (GameObject.FindObjectOfType<InputOverrideController>().IsNormal() && Input.GetButtonDown("Interact")) {
 				curLength = dialogueString.Length;
 				curDiaStr = dialogueString.Substring(0, curLength);
 				diaTextBox.GetComponent<Text>().text = curDiaStr;
