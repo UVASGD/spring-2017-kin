34ab29bdf1b363fd248e65d368946671f2a280bd
Zachary Danz <zsd4yr@virginia.edu>
2017-09-16 10:52:03 -0400

    finality

--- git-author-rewrite.sh
+++ git-author-rewrite.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="zsd4yr@users.noreply.github.com"
+OLD_EMAIL="zsd4yr@virginia.edu"
 CORRECT_NAME="Zachary Danz"
 CORRECT_EMAIL="zsd4yr@virginia.edu"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
